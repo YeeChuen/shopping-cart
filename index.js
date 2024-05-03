27277315833ce421b7ba960757af444ca8adca0d
YeeChuen <tychuen98@gmail.com>
2024-05-03 16:07:17 -0500

    all working functionality + style

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ const Model = (() => {
     #onChange;
     #inventory;
     #cart;
+
     constructor() {
       this.#inventory = []; //
       this.#cart = [];
@@ -91,17 +92,6 @@ const Model = (() => {
     set inventory(newInventory) {
       this.#inventory = newInventory;
       this.#onChange();
-    }
-
-    static increment(state, item) {
-      console.log(state.inventory);
-      const tempInventory = state.inventory.map((e, i) => {
-        if (e.content == item) {
-          e.number = parseInt(e.number) + 1;
-        }
-        return e;
-      });
-      console.log(tempInventory);
     }
     
     subscribe(cb) {
@@ -132,6 +122,8 @@ const Model = (() => {
 const View = (() => {
   // View variables
   const ulInventoryElem = document.querySelector(".inventory__list-container");
+  const ulCartElem = document.querySelector(".cart__list-container");
+  const checkoutBtn = document.querySelector(".checkout-btn");
 
   // View helper functions
   const createElement = (tag, className, id, text) => {
@@ -153,6 +145,10 @@ const View = (() => {
   const isInventoryAddCartBtn = (className) => {
     return className === "inventory__button-addcart";
   }
+  
+  const isCartDeleteBtn = (className) => {
+    return className === "cart__button-delete";
+  }
 
   const createInventoryItem = (itemName, itemNumber, itemId) => {
     const divElem = createElement("div", "inventory__item-container", itemId, undefined);
@@ -169,21 +165,53 @@ const View = (() => {
     return divElem;
   }
 
+  
+  const createCartItem = (itemName, itemNumber, itemId) => {
+    /**
+     * 
+              <div class = "cart__item-container">
+                <span class = "cart__item-name">apple x </span>
+                <span class = "cart__item-number">1</span>
+                <button class = "cart__button-delete">delete</button>
+              </div>
+     */
+    const divElem = createElement("div", "cart__item-container", itemId, undefined);
+    const spanElem1 = createElement("span", "cart__item-name", undefined, itemName);
+    divElem.appendChild(spanElem1);
+    const spanElemX = createElement("span", undefined, undefined, " x ");
+    divElem.appendChild(spanElemX);
+    const spanElem2 = createElement("span", "cart__item-number", undefined, itemNumber); // <-- number is not yet implemented
+    divElem.appendChild(spanElem2);
+    const btnElem = createElement("button", "cart__button-delete", undefined, "delete");
+    divElem.appendChild(btnElem);
+    return divElem;
+  }
+
   // implement your logic for View, View functions
   const renderInventory = (items) => {
     ulInventoryElem.innerHTML = "";
     items.forEach((e, i) => {
       ulInventoryElem.appendChild(createInventoryItem(e.content, e.number, e.id));
     })
-}
+  }
+  const renderCart = (items) => {
+    ulCartElem.innerHTML = "";
+    items.forEach((e, i) => {
+      ulCartElem.appendChild(createCartItem(e.content, e.number, e.id));
+    })
+  }
 
 
   return {
     renderInventory,
+    renderCart,
+    ulCartElem,
     ulInventoryElem,
     isInventoryAddBtn,
     isInventorySubtractBtn,
     isInventoryAddCartBtn,
+    isCartDeleteBtn,
+    checkoutBtn,
   };
 })();
 
@@ -197,6 +225,9 @@ const Controller = ((model, view) => {
         e.number = 0; // <-- track number here.
         return e;
       });
+    })
+    model.getCart().then((data) => {
+      state.cart = data;
     })
   };
 
@@ -225,29 +256,82 @@ const Controller = ((model, view) => {
   const handleAddToCart = () => {
     view.ulInventoryElem.addEventListener("click", (event) => {
       if (view.isInventoryAddCartBtn(event.target.classList[0])) { // handle add to cart button
-        console.log("add to cart");
-        console.log(state.inventory);
-        const addItem = state.inventory.filter((e) => {
-          return e.content === event.target.parentNode.firstChild.innerText && e.number > 0;
-        });
+        // if the item is not in cart.
+        const inCart = state.cart.map((e) => e.content);
+        const currId = event.target.parentNode.id; 
+        if (inCart.includes(event.target.parentNode.firstChild.innerText)) { // use PUT
+          const addedItem = {};
+          let newNumber = 0;
+          state.cart.forEach((e) => {
+            if (e.content === event.target.parentNode.firstChild.innerText) {
+              addedItem.content = e.content;
+              newNumber += e.number;
+            }
+          })
+          state.inventory.forEach((e) => {
+            if (e.content === event.target.parentNode.firstChild.innerText) {
+              newNumber += e.number;
+            }
+          })
+          addedItem.number = newNumber;
+          model.updateCart(currId, addedItem).then((data) => {
+            const tempArr = state.cart.map((e) => {
+                if (e.id.toString() !== currId) {
+                    return e
+                } else {
+                    return data;
+                }
+            });
+            state.cart = tempArr;
+          })
 
-        console.log(addItem);
+        } else { // <-- here is if the item is not in cart, use POST
+          const addItem = state.inventory.filter((e) => {
+            return e.content === event.target.parentNode.firstChild.innerText && e.number > 0;
+          });
+          if (addItem.length === 1) {
+            model.addToCart(addItem[0]).then((data) => {
+              state.cart = [data, ...state.cart];
+            })
+          }
+        }
       }
     })
   };
 
-  const handleDelete = () => {};
+  const handleDelete = () => {
+    view.ulCartElem.addEventListener("click", (event) => {
+      if (view.isCartDeleteBtn(event.target.classList[0])) {
+        const currId = event.target.parentNode.id;
+        model.deleteFromCart(currId).then(() => {
+          const tempArr = state.cart.filter((e) => {
+            return e.id.toString() !== currId;
+          });
+          state.cart = tempArr;
+        })
+      }
+    })
+  };
 
-  const handleCheckout = () => {};
+  const handleCheckout = () => {
+    view.checkoutBtn.addEventListener("click", (event) => {
+      model.checkout().then(() => {
+        state.cart = [];
+      });
+    })
+  };
 
   const bootstrap = () => {
     state.subscribe(() => {
-      view.renderInventory(state.inventory)
+      view.renderInventory(state.inventory);
+      view.renderCart(state.cart);
     });
 
     init();
     handleUpdateAmount();
     handleAddToCart();
+    handleDelete();
+    handleCheckout();
     
   };
 
